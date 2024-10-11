1abff51296f070c25657328caee0a989a00b5c1d
Mit Chin <mitchin10@gmail.com>
2024-10-10 23:32:55 -0400

    Add pagination query

--- src/controllers/getStateMetricsTimeseriesController.ts
+++ src/controllers/getStateMetricsTimeseriesController.ts
@@ -16,13 +16,56 @@ export const getStateMetricsTimeseriesController = async (
             });
         }
 
+        const pagination = Boolean(req.query.pagination as string) || false;
+
         const result = await getStateMetricsTimeseries(state.toUpperCase());
+
+        if (!pagination) {
+            res.status(200).json({
+                success: true,
+                data: result.data,
+                length: result.length,
+                error: null,
+                message: "State Metrics Timeseries Data",
+            });
+        }
+
+        const page = parseInt(req.query.page as string) || 1;
+        const limit = parseInt(req.query.limit as string) || 100;
+
+        if (limit <= 0 || page <= 0) {
+            res.status(400).json({
+                success: false,
+                data: null,
+                error: "Invalid page or limit value",
+                message: "Bad Request",
+            });
+        }
+
+        // Calculate the pagination values
+        // start and end index
+        const totalRecords = result.length;
+        const totalPages = Math.ceil(totalRecords / limit);
+        const startIndex = (page - 1) * limit;
+        const endIndex = startIndex + limit;
+
+        const paginationData = result.data.slice(startIndex, endIndex);
+
+        const paginationInfo = {
+            totalRecords,
+            totalPages,
+            recordsPerPage: paginationData.length,
+            currentPage: page,
+            hasNextPage: page < totalPages,
+            hasPrevPage: page > 1,
+        };
+
         res.status(200).json({
             success: true,
-            data: result.data,
-            length: result.length,
+            data: paginationData,
+            pagination: paginationInfo,
             error: null,
-            message: "State Metrics Timeseries Data",
+            message: "Successfully fetched State Metrics Timeseries Data",
         });
     } catch (error) {
         console.error(error);
