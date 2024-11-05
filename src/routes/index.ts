da95f962cf59f3178fc75c6b6d207f2cfbc29c50
Mit Chin <mitchin10@gmail.com>
2024-11-04 20:45:36 -0500

    create download-resume api. Cache data to localCache

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { bulkUploadController } from "../controllers/bulkUploadController";
 import { getCovidInfoDataController } from "../controllers/getCovidInfoController";
 import { getStateMetricsTimeseriesController } from "../controllers/getStateMetricsTimeseriesController";
+import { downloadResumeController } from "../controllers/downloadResumeController";
 
 const router = express.Router();
 
@@ -10,6 +11,7 @@ router.get(
     "/state-metrics-timeseries/:state",
     getStateMetricsTimeseriesController
 );
+router.get("/download-resume", downloadResumeController);
 
 router.post("/bulk-upload", bulkUploadController);
 
