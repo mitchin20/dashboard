36479c08af680b53e64e3570ee6a68047befdd13
Mit Chin <mitchin10@gmail.com>
2024-11-15 16:58:40 -0500

    console env variable

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,6 +1,9 @@
 require("dotenv").config();
 import { Redis } from "@upstash/redis";
 
+console.log("Redis URL:", process.env.UPSTASH_REDIS_REST_URL);
+console.log("Redis Token:", process.env.UPSTASH_REDIS_REST_TOKEN);
+
 export const redisClient = new Redis({
     url: process.env.UPSTASH_REDIS_REST_URL,
     token: process.env.UPSTASH_REDIS_REST_TOKEN,
